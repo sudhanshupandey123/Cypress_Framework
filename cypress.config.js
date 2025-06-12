08b8aeb54061f1aeba8dc8172273f890d8e7b433
Sudanshu Pandey <sudanshu.pandey@external.atlascopco.com>
2025-06-12 14:28:02 +0530

    updated_env_file

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,21 @@
-
+const fs = require('fs');
+const path = require('path');
 
 const { defineConfig } = require('cypress');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
 
+const envPath = path.resolve(__dirname, 'cypress.env.json');
+const envVars = JSON.parse(fs.readFileSync(envPath, 'utf8'));
+
+const viewportWidth = envVars.viewportWidth; 
+const viewportHeight = envVars.viewportHeight;
+
 module.exports = defineConfig({
   e2e: {
-      viewportWidth: 1920,
-      viewportHeight: 1080,
+      viewportWidth,
+      viewportHeight,
     video: true,
     videosFolder: 'videos',
     async setupNodeEvents(on, config) {
